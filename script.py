104e95c7b1f2135f360208d9ab0c6df3a9f74498
BeanD1pp <ssebawanzi@gmail.com>
2023-06-24 15:22:44 -0400

    made it verbose and made it only add if it doesn't already have an AI mode

--- script.py
+++ script.py
@@ -5,11 +5,27 @@ for i, line in enumerate(lines):
     model = line.startswith('MODEL=')
     line = line.replace('MODEL=', '')
     line = line.split('_')
-    
-    if model and "traffic" in line:
-        lines.insert(i + 8, "AI=fixed\n")
-    elif model:
-        lines.insert(i + 8, "AI=none\n")
+    position = i + 8
+
+    if not model:
+        continue
+
+    if position >= len(lines):
+        lines.append("\n" * 8)
+        print("added")
+
+    if "traffic" in line:
+        if not lines[position].startswith("AI=fixed"):
+            lines.insert(i + 8, "AI=fixed\n")
+            print(lines[i] + "Added: AI=fixed")
+        else:
+            print(lines[i] + "No Change: AI=fixed")
+    else:
+        if not lines[position].startswith("AI=none"):
+            lines.insert(i + 8, "AI=none\n")
+            print(lines[i] + "Added: AI=none")
+        else:
+            print(lines[i] + "No Change: AI=none")
 
 with open("entry_list.txt", "w") as file:
     file.writelines(lines)
